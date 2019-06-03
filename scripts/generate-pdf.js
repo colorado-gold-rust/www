33c53e18fe50349016654f6e3443a7aaba5475ad
Nicholas Young <nyoung@uptime.ventures>
2019-06-03 14:36:37 -0600

    Add new build scripts

---
+++ scripts/generate-pdf.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const http = require('http-server');
+
+async function createPDF(output) {
+  const server = http.createServer({
+    base: path.resolve(__dirname, '../preview'),
+  });
+
+  server.listen(7878); // Because Rust is awesome.
+
+  // Create a new instance of Chrome
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  /// Instantiate a page within the browser
+  const page = await browser.newPage();
+
+  /// Load `public/index.html`
+  await page.goto('http://127.0.0.1:7878/sponsors/packages/', {
+    waitUntil: 'networkidle0',
+  });
+
+  /// Render to PDF
+  await page.pdf({
+    path: path.resolve(__dirname, '../public', output),
+    format: 'Letter',
+  });
+
+  /// Shutdown browser instance
+  await browser.close();
+  server.close();
+}
+
+createPDF('sponsors/packages.pdf').catch(console.error);
