3cb82021f5dd0eb9d040ae7b56fdaab36ae28831
Fader <nyoung@uptime.ventures>
2019-06-07 22:29:24 -0600

    Echo Zola version on build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "Building with $(zola --version)"
 zola build -u $DEPLOY_URL -o public/ && \
 zola build -u http://127.0.0.1:7878 -o preview/ && \
 npm ci && npm run generate-pdf
