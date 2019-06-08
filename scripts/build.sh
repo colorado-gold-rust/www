276d4a79e1e672d5ebb5b12bf383798728ad26c1
Fader <nyoung@uptime.ventures>
2019-06-08 07:02:14 -0600

    Drop PDF build from scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-zola build -u $DEPLOY_URL -o public/ && \
-zola build -u http://127.0.0.1:7878 -o preview/ && \
-npm ci && npm run generate-pdf
+zola build -u $DEPLOY_URL -o public/ #&& \
+#zola build -u http://127.0.0.1:7878 -o preview/ && \
+#npm ci && npm run generate-pdf
