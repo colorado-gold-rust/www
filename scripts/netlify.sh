3418a9e6a77cc91e027b212840f3d0fea0d9a806
Fader <nyoung@uptime.ventures>
2019-06-08 17:00:03 -0600

    Add default DEPLOY_URL to Netlify build

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+DEPLOY_URL=${DEPLOY_URL:=https://www.cogoldrust.com}
 ZOLA_VERSION="0.5.1"
 BASE_URL="https://github.com/getzola/zola/releases/download"
 FILE=zola-v$ZOLA_VERSION-x86_64-unknown-linux-gnu.tar.gz
 curl -s -L $BASE_URL/v$ZOLA_VERSION/$FILE | \
 tar xzf - -C ./
+echo "Building with base URL $DEPLOY_URL..."
 ./zola build -u $DEPLOY_URL -o public/ #&& \
