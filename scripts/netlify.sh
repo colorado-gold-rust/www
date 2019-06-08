c67344b6a5ed29f6736c0509c1fd42b3a9d84ea5
Fader <nyoung@uptime.ventures>
2019-06-08 17:06:56 -0600

    Default to URL env var in Netlify builds

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-DEPLOY_URL=${DEPLOY_PRIME_URL:=https://www.cogoldrust.com}
+BASE_URL=${URL:=$DEPLOY_URL}
 ZOLA_VERSION="0.5.1"
 BASE_URL="https://github.com/getzola/zola/releases/download"
 FILE=zola-v$ZOLA_VERSION-x86_64-unknown-linux-gnu.tar.gz
 curl -s -L $BASE_URL/v$ZOLA_VERSION/$FILE | \
 tar xzf - -C ./
 echo "Building with base URL $DEPLOY_URL..."
-./zola build -u $DEPLOY_URL -o public/ #&& \
+./zola build -u $BASE_URL -o public/ #&& \
