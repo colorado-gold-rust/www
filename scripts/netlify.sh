57fc3d33032d7cfea1dc8bd0dd1f8d03b2f9b35b
Nicholas Young <nyoung@uptime.ventures>
2019-07-11 20:18:06 -0600

    Upgrade Zola to 0.8.0

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SITE_BUILD_URL=${BASE_URL:=https://www.cogoldrust.com}
-ZOLA_VERSION="0.5.1"
+ZOLA_VERSION="0.8.0"
 BASE_URL="https://github.com/getzola/zola/releases/download"
 FILE=zola-v$ZOLA_VERSION-x86_64-unknown-linux-gnu.tar.gz
 curl -s -L $BASE_URL/v$ZOLA_VERSION/$FILE | \
